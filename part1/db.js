c7876b6448ec040e5ca3b8ec9cb8bbfcbd1d9116
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 13:10:23 +0000

    [automatic commit] 2025-06-20 13:10:23 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,9 @@
 import mysql from 'mysql2'
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root'
+    user: 'root',
+    password: '',
+    database:D
 });
 
 const = mysql.createPool({
