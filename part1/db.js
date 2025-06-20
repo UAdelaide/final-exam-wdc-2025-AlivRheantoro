313d02e5ea4273cf6dc7f096d2b6d633e07b7a80
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 16:30:09 +0000

    [automatic commit] 2025-06-20 16:30:09 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -120,7 +120,7 @@ export const appPool = mysql.createPool({
     database: 'DogWalkService'
 }).promise();
 
-export async function getAPIDogs()
+export async function getAPIDogs(){
   const [rows] = await connection.query(`
     SELECT
       Dogs.name AS dog_name,
