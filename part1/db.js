31730e0188818df221e7a1aaffc95f6686ed01fd
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 16:50:06 +0000

    [automatic commit] 2025-06-20 16:50:06 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -153,16 +153,15 @@ export async function getAPIWalkerSummary() {
   const [rows] = await connection.query(`
     SELECT
       u.username AS walker_username,
-      COUNT(DISTINCT wr.request_id) AS completed_walks,
       COUNT(r.rating_id) AS total_ratings,
-      ROUND(AVG(r.rating), 1) AS average_rating
+      ROUND(AVG(r.rating), 1) AS average_rating,
+      COUNT(CASE WHEN wr.status = 'completed' THEN 1 END) AS completed_walks
     FROM Users u
-    LEFT JOIN WalkRequests wr
-      ON u.user_id = wr.walker_id AND wr.status = 'completed'
-    LEFT JOIN WalkRatings r
-      ON u.user_id = r.walker_id
+    LEFT JOIN WalkApplications wa ON u.user_id = wa.walker_id AND wa.status = 'accepted'
+    LEFT JOIN WalkRequests wr ON wa.request_id = wr.request_id
+    LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
     WHERE u.role = 'walker'
-    GROUP BY u.user_id, u.username
+    GROUP BY u.user_id;
   `);
   return rows;
 }
