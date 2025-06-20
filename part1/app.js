f91730dbda0c47537f55fe768ac47f9cf2b089a1
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 07:01:38 +0000

    [automatic commit] 2025-06-20 07:01:38 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -118,7 +118,18 @@ let db;
           (2, (SELECT user_id FROM Users WHERE username='bobwalker'), (SELECT user_id FROM Users WHERE username='carol123'), 4, 'Nice effort.')
       `);
 
-      
+      await db.execute(`
+        CREATE TABLE IF NOT EXISTS WalkApplications (
+        application_id INT AUTO_INCREMENT PRIMARY KEY,
+        request_id INT NOT NULL,
+        walker_id INT NOT NULL,
+        applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+    CONSTRAINT unique_application UNIQUE (request_id, walker_id)
+  )
+`);
     }
 
     console.log('✅ Database and seed data ready');
