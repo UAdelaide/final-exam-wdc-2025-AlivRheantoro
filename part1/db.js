b0b57fb286a4c1778cde92b6d6c6a9bdf2296759
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 16:34:20 +0000

    [automatic commit] 2025-06-20 16:34:20 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -132,7 +132,22 @@ export async function getAPIDogs(){
   return rows;
 }
 
-
+export async function getOpenWalkRequests() {
+  const [rows] = await connection.query(`
+    SELECT
+      wr.request_id,
+      d.name AS dog_name,
+      wr.requested_time,
+      wr.duration_minutes,
+      wr.location,
+      u.username AS owner_username
+    FROM WalkRequests wr
+    JOIN Dogs d ON wr.dog_id = d.dog_id
+    JOIN Users u ON d.owner_id = u.user_id
+    WHERE wr.status = 'open'
+  `);
+  return rows;
+}
 
 
 
