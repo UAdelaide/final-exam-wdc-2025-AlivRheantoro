5f172af4381517878698e8e5a21c3ecf84b45c9f
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 13:38:07 +0000

    [automatic commit] 2025-06-20 13:38:07 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql2'
+import mysql from 'mysql2';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'DogWalkService'
-}).promise()
+}).promise();
 
 async function getDogs() {
     const [rows] = await pool.query("Select * FROM Dogs");
