2af268a9061ed0b3f53d6dd24d329f3102cdea64
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 13:53:50 +0000

    [automatic commit] 2025-06-20 13:53:50 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -3,10 +3,10 @@ import express from 'express';
 const app = express();
 
 app.get("/api/dogs", (req, res) => {
-    res.send("")
-}
+    res.send("woof");
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
+    console.error(err.stack);
     res.status(500).send('Something broke!');
 });
