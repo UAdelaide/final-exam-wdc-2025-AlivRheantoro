739d237303e9cbbdf05a4bcf99c8773cc925378a
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 13:07:34 +0000

    [automatic commit] 2025-06-20 13:07:33 UTC 49.178.74.79

--- part1/db.js
+++ part1/db.js
@@ -1,11 +1,10 @@
 import mysql from 'mysql2'
-const mysql = require('mysql2/promise');
+const pool = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const  = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'DogWalkService'
 });
 
-module.exports = pool;
