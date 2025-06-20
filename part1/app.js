3af77c03316147a7ad37fdb5d8fdc5dc94980242
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 09:00:07 +0000

    [automatic commit] 2025-06-20 09:00:06 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -8,11 +8,10 @@ app.use(express.json());
 seedDatabase();
 app.use('/api', apiRoutes);
 
+const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Check Insomnia GET for API response');
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
