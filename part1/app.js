6ad5fbe8ea41c938ffb3e8a62752c224bae6cfaa
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 09:42:19 +0000

    [automatic commit] 2025-06-20 09:42:19 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
 const apiRoutes = require('./routes/api');
 const StartupDatabase = require('./StartupData');
 
-app.get('/', (req, res) => {
-    res.send('Check Insomnia GET for API response');
-});
 
 StartupDatabase();
 app.use('/api', apiRoutes);
 
 const PORT = 3000;
+app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Check Insomnia GET for API response');
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
