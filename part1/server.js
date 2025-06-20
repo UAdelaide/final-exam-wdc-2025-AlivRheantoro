0522f691e74289e4d8505272d7245a37f7706d52
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 06:17:06 +0000

    [automatic commit] 2025-06-20 06:17:06 UTC 49.178.74.79

--- part1/server.js
+++ part1/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
