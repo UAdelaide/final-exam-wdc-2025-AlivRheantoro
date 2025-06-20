0780ae7c8f8f81ac2ccb0744c5a4eb62ea5c33b0
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 10:17:36 +0000

    [automatic commit] 2025-06-20 10:17:36 UTC 49.178.74.79

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -7,9 +7,9 @@ const db = require('../db');
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT d.name AS dog_name, d.size, u.username AS owner_username
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
     `);
     res.json(rows);
   } catch (err) {
