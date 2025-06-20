6a02ac34e5af5f1c65ba18d4c35b830952f56a68
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 07:48:47 +0000

    [automatic commit] 2025-06-20 07:48:47 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const db = require('./db');
+const insertSeedData = require('./insertSeedData');
+
+// Insert data on startup
+insertSeedData();
+
+// Routes
+app.use('/api', require('./routes/dogs'));
+app.use('/api', require('./routes/walkrequests'));
+app.use('/api', require('./routes/walkers'));
+
+app.listen(port, () => {
+  console.log(`✅ Server running at http://localhost:${port}`);
+});
