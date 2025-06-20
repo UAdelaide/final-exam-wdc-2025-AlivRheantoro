3211ba8b45432a8ade6d8cb5511d7eb38ca1d1e3
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-20 13:56:35 +0000

    [automatic commit] 2025-06-20 13:56:34 UTC 49.178.74.79

--- part1/app.js
+++ part1/app.js
@@ -11,6 +11,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen, (8080, () => {
+app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
