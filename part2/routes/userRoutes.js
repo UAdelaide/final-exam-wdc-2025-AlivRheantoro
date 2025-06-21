e86a497f593c7d63884e7b9c0da2738b055213ca
AlivRheantoro <a1924315@adelaide.edu.au>
2025-06-21 06:53:09 +0000

    [automatic commit] 2025-06-21 06:53:08 UTC 49.178.74.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -55,4 +55,12 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.status(200).json({ message: 'Logged out' });
+  });
+});
+
+
 module.exports = router;
